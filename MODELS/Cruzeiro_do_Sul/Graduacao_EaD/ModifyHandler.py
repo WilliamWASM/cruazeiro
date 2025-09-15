27c716576304606d155c3d9a2e44495f36682a8c
davison queiroz <davisonvinicius@outlook.com.br>
2025-09-15 10:26:51 -0300

    divisao de classes e criação de classe geradora de avisos

---
+++ MODELS/Cruzeiro_do_Sul/Graduacao_EaD/ModifyHandler.py
@@ -0,0 +1,53 @@
+from MODELS.Cruzeiro_do_Sul.Graduacao_EaD.AdjustmentsOffersPattern import AdjustmentsOffersPattern
+from MODELS.Cruzeiro_do_Sul.Graduacao_EaD.InitialTreatments import InitialTreatments as it
+from MODELS.Cruzeiro_do_Sul.Graduacao_EaD.MspGenerate import MspGenerate as mgen
+from MODELS.excel_file.DataFrameUtils import DataFrameUtils as dfu
+import pandas as pd
+class ModifyHandler:
+    def __init__(self,offers,campus_relation,exp_campus):
+        self.offers = offers
+        self.campus_relation = campus_relation
+        self.campus = exp_campus
+        self._initial_treatments()
+        self._set_new_variables()
+
+    def _initial_treatments(self):
+        treatments = it(self.offers,self.campus_relation,self.campus)
+        self.list_treated = treatments.load()
+
+    def _set_new_variables(self):
+        self.offers = self.list_treated[0]
+        self.offers_to_campus = self.list_treated[1]
+        self.campus_group = self.list_treated[2]
+        self.campus_virtual = self.list_treated[3]
+        self.campus_offers_undefined = self.list_treated[4]
+        self.not_totally_group = self.list_treated[5]
+        self.not_totally_virtual = self.list_treated[6]
+
+    def set_values(self,enrollment_semester,end_date,special_condition):
+        adj_offers = AdjustmentsOffersPattern(self.offers,self.offers_to_campus,enrollment_semester,end_date,special_condition)
+        self.offers_to_campus = adj_offers.load()
+        if not self._verify_if_empty(self.not_totally_group):
+            adj_not_tot_group = AdjustmentsOffersPattern(self.offers,self.not_totally_group,enrollment_semester,end_date,special_condition)
+            self.not_totally_group = adj_not_tot_group.load()
+        if not self._verify_if_empty(self.not_totally_virtual):
+            adj_not_tot_virtual = AdjustmentsOffersPattern(self.offers,self.not_totally_virtual,enrollment_semester,end_date,special_condition)
+            self.not_totally_virtual = adj_not_tot_virtual.load()
+
+    def _generate_msp(self):
+        offers_msp = mgen(self.offers_to_campus,self.campus_group)
+        self.offers_generated,self.offers_virtual = offers_msp.load()
+        self.offers_generated = dfu.concat_dataframes(self.offers_generated,self.offers_virtual)
+
+    def load(self,fullpath):
+        self._generate_msp()
+        if not self._verify_if_empty(self.campus_offers_undefined):
+            dfu.save_multiple_dataframes([self.offers_generated,self.campus_offers_undefined,self.not_totally_group,self.not_totally_virtual],fullpath,
+                                         ['Ofertas para subir','campi_not_found','group_not_found','virtual_not_found'])
+        else:
+            dfu.save_multiple_dataframes([self.offers_generated,self.not_totally_group,self.not_totally_virtual],fullpath,
+                                         ['Ofertas para subir','group_not_found','virtual_not_found'])
+
+    def _verify_if_empty(self,dataframe):
+        return dataframe.empty
+        
---
+++ MODELS/Cruzeiro_do_Sul/Graduacao_EaD/MspGenerate.py
@@ -0,0 +1,78 @@
+from MODELS.excel_file.SheetManipulation import SheetManipulation as sma
+from MODELS.excel_file.DataFrameUtils import DataFrameUtils as dfu
+from MODELS.Cruzeiro_do_Sul.Graduacao_EaD import AdjustmentsOffersPattern as aop
+from MODELS.Cruzeiro_do_Sul.Graduacao_EaD.ExtraWarningGenerate import ExtraWarningGenerate as ewa
+import pandas as pd
+class MspGenerate:
+    def __init__(self,campus_offers,campus_group):
+        self.campus_offers = campus_offers
+        self.campus_group = campus_group
+        self.columns_map = {
+            'NOM_FILI':'Nome da IES',
+            'NOME_POL':'Nome do Campus',
+            'CURSO':'Nome do Curso',
+            'GRAU':'Grau',
+            'METODOLOGIA' :'Modalidade',
+            'DURAÇÃO':'Duração do Curso',
+            'PREÇO PARCELAS':'Mensalidade sem desconto',
+            'PORCENTAGEM DE DESCONTO':'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)',
+            'COD_CURS':'COD CURSO',
+            'ID_POLO':'COD CAMPUS',
+            'CÓDIGO DA IES': 'COD IES'
+        }    
+
+    def _verify_regression(self):
+        try:
+            semester = self.campus_offers.loc[3,'Semestre de Ingresso']
+            if "." in semester:
+                semester = semester.split(".")
+                enrollment = semester[1]
+                return int(enrollment)
+        except Exception:
+            raise "ERRO! Semestre de ingresso inválido"
+    
+    def _set_regression(self):
+        regression = self._verify_regression()
+        if regression ==  1:
+            self.campus_offers['Porcentagem total de desconto da bolsa\n(2º Semestre)'] = self.campus_offers['PORCENTAGEM DE DESCONTO']
+    
+    def _adjusts_discounts(self):
+        self._set_regression()
+        self.campus_offers.rename(columns=self.columns_map,inplace= True)
+        if 'Porcentagem total de desconto da bolsa\n(2º Semestre)' in self.campus_offers.columns:
+            self.campus_offers.rename(columns={'PORCENTAGEM DE DESCONTO':'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)'}, inplace=True)
+            self.campus_offers.rename(columns={'DESCONTO GARANTIDO':'Porcentagem total de desconto da bolsa\n(3º Semestre)'}, inplace=True)
+            self.campus_offers['Porcentagem de desconto IES\n(1º Semestre)'] = self._generate_formatted_percentage_column(self.campus_offers['Porcentagem de desconto da bolsa (Fixo/1 º Semestre)'])
+            self.campus_offers['Porcentagem de desconto IES\n(2º Semestre)'] = self._generate_formatted_percentage_column(self.campus_offers['Porcentagem total de desconto da bolsa\n(2º Semestre)'])
+            self.campus_offers['Porcentagem de desconto IES\n(3º Semestre)'] = self._generate_formatted_percentage_column(self.campus_offers['Porcentagem total de desconto da bolsa\n(3º Semestre)'])
+        else:
+            self.campus_offers.rename(columns={'PORCENTAGEM DE DESCONTO':'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)',
+                                                'DESCONTO GARANTIDO':'Porcentagem total de desconto da bolsa\n(2º Semestre)'}, inplace=True)
+            self.campus_offers['Porcentagem de desconto IES\n(1º Semestre)'] = self._generate_formatted_percentage_column(self.campus_offers['Porcentagem de desconto da bolsa (Fixo/1 º Semestre)'])
+            self.campus_offers['Porcentagem de desconto IES\n(2º Semestre)'] = self._generate_formatted_percentage_column(self.campus_offers['Porcentagem total de desconto da bolsa\n(2º Semestre)'])
+            
+    def _generate_formatted_percentage_column(self,series):
+        series_float = series.astype(str).str.replace(',', '.', regex=False).astype(float) 
+        series_float = series_float - 0.05
+        return series_float.map(lambda x: f"{x:.2f}")
+    
+    def _fill_in_remaining_values(self):
+        self.campus_offers = dfu.xlookup(self.campus_offers,self.campus_group,'Nome da IES','university_name','university_id','ID da IES')
+        self.campus_offers = ewa(self.campus_offers).load()
+
+    def _generate_virtual_offers(self):
+        self.offers_virtual = self.campus_offers.copy()
+        self.offers_virtual['Avisos'] = self.offers_virtual['Avisos'] + " | Certificado pela " + self.offers_virtual['Nome da IES']
+        self.offers_virtual.loc[:,'ID da IES'] = '3719'
+        self.offers_virtual.loc[:,'Nome da IES'] = 'Cruzeiro Virtual'
+
+    def _drop_extra_columns(self):
+        self.campus_offers = self.campus_offers.drop(columns=['COD_INST','ID_POLO_HUB','NOME_POLO_HUB','matches_concat','lookup_group','lookup_3719','DES_CURS'])
+        self.offers_virtual = self.offers_virtual.drop(columns=['COD_INST','ID_POLO_HUB','NOME_POLO_HUB','matches_concat','lookup_group','lookup_3719','DES_CURS'])
+
+    def load(self):
+        self._adjusts_discounts()
+        self._fill_in_remaining_values()
+        self._generate_virtual_offers()
+        self._drop_extra_columns()
+        return self.campus_offers,self.offers_virtual
