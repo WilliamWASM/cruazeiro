e82d976eab04f3a5353a0d1dabe864cc940a264c
davison queiroz <davisonvinicius@outlook.com.br>
2025-10-15 13:50:40 -0300

    added controller and imports

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.gui.views.main_window import *
 from GUI.widgets.cards import *
-
+from src.gui.controllers.main_window_controller import *
+import sys
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.setup_config(1200,700,700,400,"Teste Application")
+    controller = MainWindowController(window)
+    window.show()
     sys.exit(app.exec())
