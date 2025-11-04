f1f97b4b025e65d4b5c352c902163319664f798f
davison queiroz <davisonvinicius@outlook.com.br>
2025-11-04 13:59:36 -0300

    fix in key used of dictionaire and rename variable

--- src/gui/controllers/card_controller.py
+++ src/gui/controllers/card_controller.py
@@ -49,11 +49,12 @@ class CardController():
         
         require_input = self.action_config.get("requires_input", False)
         if require_input:
-            inputs = self.action_config.get("user_inputs", [])
-            user_inputs = self.get_user_inputs(inputs, "Entradas Necessárias")
+            fields = self.action_config.get("inputs", [])
+            user_inputs = self.get_user_inputs(fields, "Entradas Necessárias")
             if not user_inputs:
                 Notification.error("Cancelado", "Processo cancelado pelo usuário.")
                 return
+            
             data["user_inputs"] = user_inputs
             
         save_type = self.action_config.get("save_type","save_file")
