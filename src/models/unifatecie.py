ef11d6f4d9fad252d86f49983098dcd721969eb4
davison queiroz <davisonvinicius@outlook.com.br>
2025-11-10 13:51:21 -0300

    fix import statements to use relative paths in unifatecie.py

--- src/models/unifatecie.py
+++ src/models/unifatecie.py
@@ -1,5 +1,5 @@
-from models.excel_file.SheetManipulation import SheetManipulation as sma
-from models.excel_file.DataFrameUtils import DataFrameUtils as dfu
+from .excel_file.SheetManipulation import SheetManipulation as sma
+from .excel_file.DataFrameUtils import DataFrameUtils as dfu
 import pandas as pd
 import os
 from datetime import date
